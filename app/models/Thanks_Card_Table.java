2e35b2a11da9ab84e8adadf685c997cad364080b
scarlet1237 <yuu.scarletrosemallow.1205@gmail.com>
2016-05-30 10:08:13 +0900

    Add files via upload

---
+++ app/models/Thanks_Card_Table.java
@@ -0,0 +1,23 @@
+package models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import com.avaje.ebean.Model;
+
+
+@Entity
+public class Thanks_Card_Table extends Model {
+	@Id
+	public Integer card_id;
+	public Integer send_user_id;
+	public Integer receive_user_id;
+	public String help_content;
+	public String sent_content;
+	public String today;
+	public String rank;
+	public boolean read_card;
+	public boolean check_flg;
+
+}
